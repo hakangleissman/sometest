566ec9edab5742455f432f83fcd066e37a33281c
hakangleissman <hakan.gleissman@gmail.com>
2024-05-06 12:03:46 +0200

    first commit

---
+++ src/test/java/se/test/MainTest.java
@@ -0,0 +1,11 @@
+package se.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testSome(){
+        Assertions.assertEquals(1, 0);
+    }
+}
